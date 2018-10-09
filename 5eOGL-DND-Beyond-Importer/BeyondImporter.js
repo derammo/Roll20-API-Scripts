7d2fdf29e16e4650536fee6f331a9653b6a1f734
Ammo Goettsch <ammo@derammo.net>
2018-10-09 19:15:10 -0400

    styling adjusted to not clip chat dialog on left side

--- 5eOGL-DND-Beyond-Importer/BeyondImporter.js
+++ 5eOGL-DND-Beyond-Importer/BeyondImporter.js
@@ -34,7 +34,7 @@
     let object;
 
     // Styling for the chat responses.
-    const style = "margin-left: -45px; overflow: hidden; background-color: #fff; border: 1px solid #000; padding: 5px; border-radius: 5px;";
+    const style = "margin-left: 0px; overflow: hidden; background-color: #fff; border: 1px solid #000; padding: 5px; border-radius: 5px;";
     const buttonStyle = "background-color: #000; border: 1px solid #292929; border-radius: 3px; padding: 5px; color: #fff; text-align: center; float: right;"
 
     let jack = '0';
